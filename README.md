d8b4143bc9dcec394b758492cefc4154ed4c39e6
Enrique W <kique10000@gmail.com>
2013-12-27 23:42:22 -0500

    Refactored and made ball random

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ BallAround
 
 A Simple sample animation using vanilla javascript and CSS3 Keyframes  
 (Alternative from using "canvas" in js)  
-Checkout the <a href="http://kiquedev.github.io/BallAround/">Application HERE</a>
+Checkout the [Application Here](http://kiquedev.github.io/BallAround/)
--- client.js
+++ client.js
@@ -1,44 +1,121 @@
 (function (root) {
   "use strict";
+
   var BallAround = function () {
+    // Set this to self var ncase of scope problems
     var self = this;
 
-    self.size = 60;// Defaults size
+    // Defaults ballSize is 60px
+    self.ballSize = 60;
+    // Get #content element selector
     self.contentDiv = document.querySelector("#content");
 
-    self.setSettings = function(ballDiv, size){
-      if(typeof size !== 'number')
-        return console.log("Size must be a number");
+    // Function that Set Settings for BallAround
+    self.SetSettings = function(ballDiv, ballSize){
+      // Check if ballSize is an Object Number
+      if(typeof ballSize !== 'number')
+        return console.log("Error: Size must be a number");
 
-      ballDiv.style.width = size.toString()+"px";
-      ballDiv.style.height = size.toString()+"px";
-      self.contentDiv.style.top = size.toString()+"px";
-      self.contentDiv.style.bottom = size.toString()+"px";
-      self.contentDiv.style.right = size.toString()+"px";
+      // Set with and height of the ball
+      ballDiv.style.width = ballSize.toString()+"px";
+      ballDiv.style.height = ballSize.toString()+"px";
     }
 
-    self.makeBall  = function(size) {
-      var ballDiv = document.createElement("div");
-      ballDiv.classList.add("ball");
-      ballDiv.classList.add("moveBall");
+    // Function that Creates Random Position of the ball
+    self.RandomPosition = function(ballSize){
+      var height = window.innerHeight - (ballSize+60); // Additional 60 due to the header
+      var width = window.innerWidth - ballSize;
 
+      var newHeight = Math.ceil(Math.random() * height);
+      var newWidth = Math.ceil(Math.random() * width);
+
+      var pos = { x: newWidth , y: newHeight }
+
+      return pos;   
+    }
+
+    // Function that Calculates the speed of the ball
+    // self.CalculateSpeed = function(oldPos, newPos){
+    //   var x = Math.abs(oldPos.x - newPos.x);
+    //   var y = Math.abs(newPos.y - newPos.y);
+      
+    //   var greatest = x > y ? x : y;
+      
+    //   var speedModifier = 100;
+
+    //   var speed = Math.ceil(greatest/speedModifier);
+
+    //   return speed;   
+    // }
+
+    self.MoveRandom = function (ballDiv, ballSize){
+      var transition = function(e) {
+        // Stop other listener such as parent
+        e.stopPropagation();
+        // Make sure transition runs once since it runs for all set of transition made "top, left, width, height"
+        if(e.propertyName === "width" || e.propertyName === "top") {
+
+          // Create random position for the ball
+          var randomPos = self.RandomPosition(ballSize);
+          
+          // Get ball div element offset
+          var offset = ballDiv.getBoundingClientRect();
+          var currentPos = { x: offset.left, y: offset.top }
+
+          // Set speed transition 4 second for now (Might change it so user can set it)
+          ballDiv.style.transitionDuration = "4s";
+
+          // Set top and left transition
+          ballDiv.style.left = randomPos.x.toString()+"px";
+          ballDiv.style.top = randomPos.y.toString()+"px";
+
+        }
+      }
+      
+      // Event Listener for across browser when transition end callback function "transition"
+      ballDiv.addEventListener("webkitTransitionEnd", transition, false);
+      ballDiv.addEventListener("transitionend", transition, false);
+      ballDiv.addEventListener("oTransitionEnd", transition, false);
+
+    }
+
+    // Function that Creates a Ball
+    self.CreateBall  = function(ballSize) {
+      // Create the ball element
+      var ballDiv = document.createElement("div");
+      // Add Class .ball
+      ballDiv.classList.add("ball");
+
+      // If ballSize params exist set self.ballSize to ballSize params else set to default size (60)
+      if(ballSize) self.ballSize = ballSize;
+    
+      // Append ball Element to #Content 
       self.contentDiv.appendChild(ballDiv);
 
-      window.getComputedStyle(ballDiv).width; // Rerender the transition animation after dynamic alloc
+      // Rerender the width transition animation after dynamic alloc using "Compute Style"
+      window.getComputedStyle(ballDiv).width;
 
-      (size) ? self.setSettings(ballDiv, size) : self.setSettings(ballDiv, self.size);
+      // Set Ball Size
+      self.SetSettings(ballDiv, self.ballSize);
 
+      // Make Ball Move Random
+      self.MoveRandom(ballDiv, self.ballSize);
+
+      // Add event handler for each ball selector.
+      // If clicked it creates another clone of the ball
       ballDiv.addEventListener("click", function() {
         console.log("Ball Clicked");
-        self.makeBall(size);
+        // Create Balll
+        self.CreateBall(self.ballSize);
       });
-
     }
+
   };
 
-  // Init Function  
-  BallAround.prototype.init = function (size) {
-    this.makeBall(size);
+  // Function that initialize BallAround 
+  BallAround.prototype.init = function (ballSize) {
+    // Create Ball
+    this.CreateBall(ballSize);
   };
 
   root.BallAround = BallAround;
@@ -46,7 +123,7 @@
 }(this));
 
 // Creation
-var d = new BallAround();
-// d.init(40); // Custom Ball size
-d.init();
+var b = new BallAround();
+//b.init(40); // Custom Ball Size
+b.init();
 
